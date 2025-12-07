39aad7a02f5a2424104f24da6d6f7ee4ae62e5f6
Oscar Castellanos <oscareliascastellanoscordero@gmail.com>
2025-12-07 17:16:47 -0600

    Animaciones 101 - Basicas - FadeInd, FadeOut

--- presentation/shared/ThemedView.tsx
+++ presentation/shared/ThemedView.tsx
@@ -16,7 +16,7 @@ const ThemedView = ({
     children,
 }: Props) => {
   return (
-    <View className={`bg-light-background dark:bg-dark-background ${className}`}>
+    <View className={`bg-light-background dark:bg-dark-background mx-3 ${className}`}>
       {children}
     </View>
   );
