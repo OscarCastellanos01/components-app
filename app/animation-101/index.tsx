aa3279c28cc42ff3ecbceb7dac9ecf5cca247f62
Oscar Castellanos <oscareliascastellanoscordero@gmail.com>
2025-12-07 17:25:25 -0600

    Animaciones 101 - Movimiento - FadeInd, FadeOut

--- app/animation-101/index.tsx
+++ app/animation-101/index.tsx
@@ -1,4 +1,4 @@
-import { Animated } from 'react-native';
+import { Animated, Easing } from 'react-native';
 
 import ThemedButton from '@/presentation/shared/ThemedButton';
 import ThemedView from '@/presentation/shared/ThemedView';
@@ -7,12 +7,21 @@ import { useRef } from 'react';
 const Animation101Screen = () => {
 
   const animatedOpacity = useRef(new Animated.Value(0)).current;
+  const animatedTop = useRef(new Animated.Value(-100)).current;
 
   const fadeIn = () => {
     Animated.timing(animatedOpacity, {
       toValue: 1,
       duration: 300,
       useNativeDriver: true,
+    }).start();
+
+    Animated.timing(animatedTop, {
+      toValue: 0,
+      duration: 700,
+      useNativeDriver: true,
+      // easing: Easing.elastic(2),
+      easing: Easing.bounce,
     }).start();
   }
 
@@ -21,7 +30,8 @@ const Animation101Screen = () => {
       toValue: 0,
       duration: 300,
       useNativeDriver: true,
-    }).start();
+    // }).start(() => animatedTop.setValue(-100));
+    }).start(() => animatedTop.resetAnimation());
   }
 
   return (
@@ -32,6 +42,11 @@ const Animation101Screen = () => {
           width: 150,
           height: 150,
           opacity: animatedOpacity,
+          transform: [
+            {
+              translateY: animatedTop,
+            },
+          ],
         }}
       />
 
