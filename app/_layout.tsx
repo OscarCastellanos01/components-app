875709cf578897771ce30c589a7026b2d86cc6c0
Oscar Castellanos <oscareliascastellanoscordero@gmail.com>
2025-12-07 12:38:14 -0600

    Nativewind y Paleta de colores

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,11 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/use-color-scheme';
+
+import { Text, View } from 'react-native';
+import "../global.css";
 
 export const unstable_settings = {
   anchor: '(tabs)',
@@ -14,10 +16,9 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
+      <View className=' bg-light-background dark:bg-dark-background'>
+        <Text className='mt-10 text-3xl text-light-primary dark:text-dark-primary'>Hola Mundo</Text>
+      </View>
       <StatusBar style="auto" />
     </ThemeProvider>
   );
